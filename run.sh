24cff387404867a7f3f539f4a854b270e4e55c10
Lorenzo Affetti <lorenzo.affetti@gmail.com>
2015-11-19 10:35:55 +0100

    [entrypoint] fix to see stdout

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 start-local-streaming.sh
+flink run "$@" debs15-flink.jar &
+
 echo "Connect to http://localhost:48081/#/jobmanager/stdout"
-echo "to see the standard output"
-flink run "$@" debs15-flink.jar
-/bin/bash # stay attached
+echo "to see the standard output, or watch here."
+
+sleep 2
+
+# To see logs
+tail -f $FLINK_HOME/log/flink-*.out
