18f76741d5bb962f0ef99f72a5fc7458c5c345ec
Lorenzo Affetti <lorenzo.affetti@gmail.com>
2015-11-23 17:56:44 +0100

    [fix] we have to sleep before reading output file

--- run.sh
+++ run.sh
@@ -5,5 +5,7 @@ flink run "$@" debs15-flink.jar &
 echo "Connect to http://localhost:48081/#/jobmanager/stdout"
 echo "to see the standard output, or watch here."
 
+sleep 2
+
 # To see logs
 tail -f $FLINK_HOME/log/flink-*.out
